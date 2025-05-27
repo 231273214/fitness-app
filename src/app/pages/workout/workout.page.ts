e318c58b463a35e5b7ade3647e4682b6e5c9bdb2
AxelPriet <axeprieto@uniboyaca.edu.co>
2025-05-27 01:47:36 -0500

    Arreglos finales workout page

--- src/app/pages/workout/workout.page.ts
+++ src/app/pages/workout/workout.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { TimerComponent } from 'src/app/shared/timer/timer.component';
 import { ExercisesService, Exercise } from 'src/app/core/exercises.service';
 
@@ -25,11 +26,16 @@ export class WorkoutPage {
 
   constructor(
     private alertController: AlertController,
-    private exercisesService: ExercisesService
+    private exercisesService: ExercisesService,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.availableExercises = this.exercisesService.getAll();
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']); // Ajusta la ruta según tu configuración
   }
 
   async addExercise() {
@@ -90,6 +96,36 @@ export class WorkoutPage {
     } else {
       set.restTimer = null;
     }
+  }
+
+  deleteSet(exerciseIndex: number, setIndex: number) {
+    // Verificar si es la última serie y mostrar confirmación
+    if (this.currentExercises[exerciseIndex].sets.length === 1) {
+      this.presentDeleteLastSetAlert(exerciseIndex);
+    } else {
+      this.currentExercises[exerciseIndex].sets.splice(setIndex, 1);
+    }
+  }
+
+  async presentDeleteLastSetAlert(exerciseIndex: number) {
+    const alert = await this.alertController.create({
+      header: 'Confirmar',
+      message: '¿Estás seguro de que quieres borrar la última serie? Esto eliminará el ejercicio completo.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Borrar',
+          handler: () => {
+            this.currentExercises.splice(exerciseIndex, 1);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
   finishWorkout() {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { addIcons } from 'ionicons';
+import { home, checkmark, close } from 'ionicons/icons';
+
+addIcons({
+  home,
+  checkmark,
+  close
+});
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
